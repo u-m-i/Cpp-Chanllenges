e39948b2dfc752322cb2c8922fdda182682d17e3
Miguel Reyes <asiessoyeltier1@gmail.com>
2023-09-07 18:42:46 -0500

    [challenge] Sum of N natural numbers

---
+++ FlowControl/sum_n_natural_numbers.cpp
@@ -0,0 +1,21 @@
+/**
+ * Sum the consequitive sum of n natural numbers
+ */
+
+#include<iostream>
+
+int main()
+{
+
+
+	int user_number;
+
+	std::cin >> user_number;
+
+
+	std::cout << (user_number * (user_number + 1)/2) << std::endl;
+
+
+	return 0;
+
+}
