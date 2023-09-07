1f23e0d004658d607b97e0565bc9db6c136d3f9b
Miguel Reyes <asiessoyeltier1@gmail.com>
2023-09-06 22:37:34 -0500

    [challenge] Print three sentences

---
+++ FlowControl/three_sentences.cpp
@@ -0,0 +1,20 @@
+/**
+ * Print only three times the sentence  "C++ programming is fun"
+*/
+
+#include<iostream>
+
+int main()
+{
+
+	for(int i = 0; i < 3; i++)
+	{
+
+		std::cout << "C++ Programming is fun" << '\n';
+	}
+
+	return 0;
+}
+
+
+
